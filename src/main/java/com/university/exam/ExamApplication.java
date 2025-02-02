2c7e7b985ea3bde2900ce5884b9a6c1155ad2cdb
khaled-taha <khaledtaha105@gmail.com>
2025-02-02 04:20:26 +0200

    docker compose & war version

--- src/main/java/com/university/exam/ExamApplication.java
+++ src/main/java/com/university/exam/ExamApplication.java
@@ -2,12 +2,18 @@ package com.university.exam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ExamApplication {
+public class ExamApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ExamApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExamApplication.class, args);
 	}
-
 }
