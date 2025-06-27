d0280ae2a0f9263c20f1ae23a5de90613afc77cb
Khaled Taha <khaledtaha105@gmail.com>
2025-06-27 22:54:24 +0300

    fix

--- src/main/java/com/university/exam/userManagement/dtos/responseDTO/AdminResponseDTO.java
+++ src/main/java/com/university/exam/userManagement/dtos/responseDTO/AdminResponseDTO.java
@@ -5,15 +5,18 @@ import com.university.exam.userManagement.entities.Admin;
 import lombok.Data;
 
 import java.util.List;
+import java.util.UUID;
 
 @Data
 public class AdminResponseDTO {
     private UserResponseDTO userResponseDTO;
+    private UUID adminId;
     private SpecializationResponseDTO specializationResponseDTO;
 
     public static AdminResponseDTO fromEntity(Admin admin) {
         AdminResponseDTO responseDTO = new AdminResponseDTO();
         responseDTO.setUserResponseDTO(UserResponseDTO.convertToUserResponseDTO(admin.getUser()));
+        responseDTO.setAdminId(admin.getAdminId());
         responseDTO.setSpecializationResponseDTO(SpecializationResponseDTO.convertToSpecializationResponseDTO(admin.getSpecialization()));
         return responseDTO;
     }
