51b61b65f0d67aeeaad804876b915f8bfbab1f3d
Khaled Taha <khaledtaha105@gmail.com>
2025-06-27 23:38:37 +0300

    Get all students of a specific academic year

--- src/main/java/com/university/exam/academicManagement/repos/StudentEnrollmentRepository.java
+++ src/main/java/com/university/exam/academicManagement/repos/StudentEnrollmentRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -26,4 +27,6 @@ public interface StudentEnrollmentRepository extends JpaRepository<StudentEnroll
             @Param("academicYearGroupId") UUID academicYearGroupId,
             @Param("termId") UUID termId
     );
+
+    List<StudentEnrollment> findByAcademicYearGroup_AcademicYear_IdAndEnrollmentStatus(UUID academicYearId, StudentEnrollment.EnrollmentStatus status);
 }
