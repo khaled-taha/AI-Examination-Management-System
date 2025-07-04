4ffd861e6853b067e382b892473c60a652cdb044
Khaled Taha <khaledtaha105@gmail.com>
2025-07-04 20:37:12 +0300

    add id

--- src/main/java/com/university/exam/examManagement/dtos/response/LanguagesResponseDTO.java
+++ src/main/java/com/university/exam/examManagement/dtos/response/LanguagesResponseDTO.java
@@ -3,14 +3,18 @@ package com.university.exam.examManagement.dtos.response;
 import com.university.exam.examManagement.entities.ProgrammingLanguage;
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
 public class LanguagesResponseDTO {
+    private UUID id;
     private String name;
     private String codeName;
     private String version;
 
     public static LanguagesResponseDTO fromEntity(ProgrammingLanguage language){
         LanguagesResponseDTO languagesResponseDTO = new LanguagesResponseDTO();
+        languagesResponseDTO.setId(language.getId());
         languagesResponseDTO.setName(language.getName());
         languagesResponseDTO.setCodeName(language.getCodeName());
         languagesResponseDTO.setVersion(language.getVersion());
